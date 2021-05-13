fd42e999be2c0afd3d4fd9a8c3e475ea72df2f57
shriphad <shriphadnike@gmail.com>
2021-05-13 19:59:32 +0530

    Working

--- main.py
+++ main.py
@@ -5,8 +5,32 @@ import pandas as pd
 from sklearn import datasets
 import st_state_patch as st_state
 import SessionState
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import pickle
 
-s = SessionState.get(button=False)
+s = SessionState.get(button=False,button2=False)
+#s2 = SessionState.get(button2=False)
+
+def MLmodel(gender,married,self_employed,Dependents,Applicant_Income,Co_Applicant_Income,Loan_Amount,Loan_Amount_term,Property_area,Credit_History,education):
+    data = [{'Gender': gender, 'Married': married, 'Dependents':Dependents, 'Education': education, 'Self_Employed': self_employed, 'ApplicantIncome':Applicant_Income,'CoapplicantIncome': Co_Applicant_Income, 'LoanAmount': Loan_Amount, 'Loan_Amount_Term':Loan_Amount_term,'Credit_History': Credit_History, 'Property_Area': Property_area}]
+    df = pd.DataFrame(data)
+    df.Gender.replace(('Male', 'Female'), (1, 0), inplace=True)
+    df.Dependents.replace(('0', '1', '2', '3+'), (0, 0.25, 0.5, 1), inplace=True)
+    df.Married.replace(('Yes', 'No'), (1, 0), inplace=True)
+    df.Education.replace(('Graduate', 'Not Graduate'), (1, 0), inplace=True)
+    df.Self_Employed.replace(('No', 'Yes'), (1, 0), inplace=True)
+    df.Property_Area.replace(('Urban', 'Semiurban',  'Rural'), (1, 0.5, 0), inplace=True)
+    filename = 'model.sav'
+    model = pickle.load(open(filename, 'rb'))
+    res = model.predict(df)
+    res = res.tolist()
+    print(res[0])
+    if(res[0]==0):
+        st.write("Eligible")
+    else:
+        st.write("Not Eligible")
+
 
 def preprocess(name, age, cibil):
     if(type(name) == str):
@@ -22,20 +46,28 @@ def preprocess(name, age, cibil):
 def preprocessML():
     gender = st.selectbox('Gender',('Male', 'Female'))
     married = st.selectbox('Are You Married?',('Yes', 'No'))
+    education = st.selectbox('Select your Education',('Graduate', 'Not Graduate'))
     self_employed = st.selectbox('Are You Self Employed?',('Yes', 'No'))
-    Applicant_Income = st.number_input("Applicant Income", )
-    Co_Applicant_Income = st.number_input("Co-Applicant Income (If no Co-Applicant enter Zero)", )
-    Loan_Amount = st.number_input("Please enter your Loan Amount", )
-    Loan_Amount_term = st.number_input("Please enter your Loan Amount term in days", )
+    Dependents = st.selectbox('How many Dependents do you have?',('0','1', '2','3+'))
+    Applicant_Income = st.number_input("Applicant Income (In Month)", 0)
+    Co_Applicant_Income = st.number_input("Co-Applicant Income (If no Co-Applicant enter Zero)", 0)
+    Loan_Amount = st.number_input("Please enter your Loan Amount", 0)
+    Loan_Amount_term = st.number_input("Please enter your Loan Amount term in days", 0)
     Property_area = st.selectbox('Please select your Area',('Urban', 'Semiurban','Rural'))
-    Credit_History = st.selectbox('Do you have Credit History',('Yes', 'No'))
+    Credit_History = st.selectbox('Do you have pending Credit History',('Yes', 'No'))
     if Credit_History=='Yes':
         Credit_History=1
     else:
         Credit_History=0
     
-    st.write(gender," ",married," ",self_employed," ",Applicant_Income," ",Co_Applicant_Income," ",Loan_Amount,
-        " ",Loan_Amount_term," ",Property_area," ",Credit_History," ")
+    if st.button("Run Model") or s.button2:
+        s.button2 = True
+        #st.write(gender," ",married," ",self_employed," ",Applicant_Income," ",Co_Applicant_Income," ",Loan_Amount,
+        #    " ",Loan_Amount_term," ",Property_area," ",Credit_History," ")
+        #run model
+        MLmodel(gender,married,self_employed,Dependents,Applicant_Income,
+            Co_Applicant_Income,Loan_Amount,Loan_Amount_term,Property_area,Credit_History,education)
+
 
 def basicEligibility(name, age, cibil):
     if int(age) > 18:
@@ -51,7 +83,7 @@ def main():
     user_input_1 = st.text_input("Name", )
     user_input_2 = st.text_input("Age", )
     user_input_3 = st.text_input("CIBIL Score", )
-    if st.button("Submit") or s.button:
+    if st.button("Check Basic Eligibility") or s.button:
         s.button = True
         preprocess(user_input_1, user_input_2, user_input_3)
 
